9956fe172f1074b85c67f8e895e2434e854207d1
Tobias Oberstein <tobias.oberstein@tavendo.de>
2014-10-18 12:53:22 +0200

    bump version

--- autobahntestsuite/setup.py
+++ autobahntestsuite/setup.py
@@ -84,7 +84,7 @@ setup (
    url = 'http://autobahn.ws/testsuite',
    platforms = ('Any'),
    install_requires = ['setuptools',
-                       'autobahn[twisted,accelerate]>=0.9.0',
+                       'autobahn[twisted,accelerate]>=0.9.2',
                        'jinja2>=2.6',
                        'markupsafe>=0.19',
                        'Werkzeug>=0.9.4',
